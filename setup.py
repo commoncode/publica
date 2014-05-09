16a013b72dab4477560f97c63dc71357cbed1162
Y3K <dboy.y3k@gmail.com>
2014-05-09 01:54:40 -0500

    Fix Versions

--- setup.py
+++ setup.py
@@ -29,8 +29,8 @@ setup( name='publica',
     ],
     dependency_links = [
         'http://github.com/commoncode/entropy/tarball/master#egg=entropy-0.0.3',
-        'http://github.com/commoncode/menus/tarball/master#egg=menus-0.0.2',
-        'http://github.com/commoncode/pages/tarball/master#egg=pages-0.0.2',
+        'http://github.com/commoncode/menus/tarball/master#egg=menus-0.0.3',
+        'http://github.com/commoncode/pages/tarball/master#egg=pages-0.0.3',
         'http://github.com/commoncode/platforms/tarball/master#egg=platforms-0.0.3',
         'http://github.com/commoncode/posts/tarball/master#egg=posts-0.0.2',
     ]
